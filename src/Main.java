bbeacdecd63abc9db344bfd572eac3d8a1c332ea
Marco Worni <marco.worni@students.fhnw.ch>
2024-03-15 11:08:11 +0100

    quick refactor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import fileio.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
         File file = new File();
         String content = file.readFile("res/chiffre.txt");
         System.out.println(content);
--- src/fileio/File.java
+++ src/fileio/File.java
@@ -1,14 +1,19 @@
+package fileio;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class File {
+    private static final Logger LOGGER = Logger.getLogger(File.class.getName());
     public String readFile(String filePath) {
         String content = "";
         try {
             content = new String(Files.readAllBytes(Paths.get(filePath)));
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "An IO exception occurred while reading the file.", e);
         }
         return content;
     }
